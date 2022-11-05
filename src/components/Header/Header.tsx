584db90358d387c7361e4877f4c1fa8ffaeb05f3
Eduardo Mausa <mausaeduardo@gmail.com>
2022-11-05 11:30:59 -0300

    feat: create header and home first section

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import React from 'react';
+import styles from './header.module.scss';
+
+export function Header() {
+  return (
+    <div className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <h1 className={styles.title}>
+          <Link href='/'>Mausa Blog</Link>
+      </h1>
+      <nav className={styles.navContainer}>
+        <Link className={styles.styledLink} href='/'>Home</Link>
+        <Link className={styles.styledLink} href='/posts'>Posts</Link>
+        <Link className={styles.styledLink} href='/about'>About</Link>
+      </nav>
+
+      <div className={styles.buttonContainer}>
+        <button className={styles.button}></button>
+        <button className={styles.button}></button>
+      </div>
+    </div>
+    </div >
+  );
+}
