1c699e6e21ea7959c67e745caa908d168086e95d
Eduardo Mausa <mausaeduardo@gmail.com>
2022-11-29 17:38:53 -0300

    fix: fix accent in the about page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -6,7 +6,7 @@ export default function About() {
     <main className={styles.contentContainer}>
       <h1>About me</h1>
       <section className={styles.sectionContent}>
-        <p>I'm a software developer living in Brazil. My journey in the tech industry started back in my high school</p>
+        <p>I&apos;m a software developer living in Brazil. My journey in the tech industry started back in my high school</p>
       </section>
     </main>
   );
