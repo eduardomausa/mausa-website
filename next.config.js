db62ff4a39469cc1cacf6fa0f4b6f4d90996ed09
Eduardo Mausa <mausaeduardo@gmail.com>
2023-03-02 21:23:51 -0300

    feat: create AboutMe component with lorem

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-};
+}
 
-module.exports = {
-  nextConfig,
-  pageExtensions: ["*.tsx", "*.ts"],
-};
+module.exports = nextConfig
--- src/pages/about/about.style.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const StyledAbout = styled.div`
-`;
-
-export const AboutMeSection = styled.section`
-  padding: 16px 32px;
-  text-align: justify;
-`;
