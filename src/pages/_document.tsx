c896d975fd142e9a1652e7408b100ab0ba0cccd6
Eduardo Mausa <mausaeduardo@gmail.com>
2023-03-05 15:51:24 -0300

    chore: change the _document file back to what it was

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,30 @@
-import { Html, Head, Main, NextScript } from "next/document";
-
-export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@400;500;600;700&family=Montserrat:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="shortcut icon" href="/favicon-white.ico" type="image/png" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from "../../node_modules/next/document";
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <
+            href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@400;500;600;700&family=Montserrat:wght@400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            rel="shortcut icon"
+            href="/favicon-white.ico"
+            type="image/png"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
