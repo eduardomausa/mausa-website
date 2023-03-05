c5ea677df8dd53a4cfaead1b2739e8ae21734d2d
Eduardo Mausa <mausaeduardo@gmail.com>
2023-03-05 15:52:57 -0300

    chore: change the _document file back to what it was

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,7 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <
+          <link
             href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@400;500;600;700&family=Montserrat:wght@400;500;600;700&display=swap"
             rel="stylesheet"
           />
