663f946bada788fad16f15defac8d7403f26ce42
Mausa <mausaeduardo@gmail.com>
2023-04-01 12:48:23 -0300

    refactor: remove hamburguer menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,10 @@
 import styles from "./styles.module.scss";
 import Menu from "../Menu";
-import HamburguerMenu from "../HamburguerMenu";
 
 export default function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <HamburguerMenu />
-        <h1 className={styles.title}>Eduardo Mausa</h1>
         <Menu />
       </div>
     </header>
