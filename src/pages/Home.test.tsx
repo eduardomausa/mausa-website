3f61bcdb61ed4b9ada48317b1d01b25b11a17a5b
Eduardo Mausa <emausa@mmb026002bhz.local>
2023-04-30 18:30:42 -0300

    test: create unit test for the Home component

---
+++ src/pages/Home.test.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Home from "../pages/index";
+import "@testing-library/jest-dom/extend-expect";
+
+describe("Home component", () => {
+  it("should render the Header component", () => {
+    render(<Home />);
+    const header = screen.getByRole("banner");
+    expect(header).toBeInTheDocument();
+  });
+
+  it("should render the HomeDescription component", () => {
+    render(<Home />);
+    const homeDescription = screen.getByRole("main");
+    expect(homeDescription).toBeInTheDocument();
+  });
+
+  it("should render the Footer component", () => {
+    render(<Home />);
+    const footer = screen.getByRole("contentinfo");
+    expect(footer).toBeInTheDocument();
+  });
+});
